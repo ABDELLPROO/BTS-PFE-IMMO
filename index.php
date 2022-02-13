91650dc1393af429b9e6343d413a77eb467421ca
elkarouani <elkarouani@gmail.com>
2022-02-13 18:24:56 +0100

    Configure Tailwind CSS Fixes #6

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
-echo "hello karyan nakhla ";
-
+  echo "hello karyan nakhla ";
 ?>
+
+<html>
+  <head>
+    <link rel="stylesheet" href="index.css">
+
+    <title>Home</title>
+  </head>
+
+  <body>
+    <p class="text-lg text-green-500">Hello Tailwind CSS</p>
+  </body>
+</html>
