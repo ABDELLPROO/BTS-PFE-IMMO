440069015a2cb5d27b2f9f41eeabeff3297edb62
abderrahmane jaafar <92488216+ABDELLPROO@users.noreply.github.com>
2022-06-14 21:56:42 +0100

    Delete dispatcher.php

--- dispatcher.php
+++
@@ -1,8 +0,0 @@
-<?php 
-  $_CURRENT_ROUTE = array_shift(
-    array_filter(
-      json_decode(file_get_contents('./router.json'), true), 
-      function($route) { return $route['uri'] == $_SERVER['REQUEST_URI']; }
-    )
-  );
-?>
