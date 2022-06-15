fb403d7106eb1de2224293bce8a9fd7fb3429baa
abderrahmane jaafar <92488216+ABDELLPROO@users.noreply.github.com>
2022-06-15 01:16:40 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,php}",
+    "./layouts/**/*.{html,js,php}",
+    "./pages/**/*.{html,js,php}"
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        Heebo: "'Heebo', sans-serif ",
+        Mukta: "'Mukta', sans-serif ",
+        Bebas: "'Bebas Neue', cursive",
+        Montserrat: "'Montserrat', sans-serif",
+        Oswald: "'Oswald', sans-serif",
+        Exo: "'Exo', sans-serif",
+        Metrophobic: "'Metrophobic', sans-serif",
+        Quicksand: "'Quicksand', sans-serif",
+        Maven: "'Maven Pro', sans-serif",
+        Padauk: "'Padauk', sans-serif",
+        Darker_Grotesque: "'Darker Grotesque', sans-serif",
+      },
+    },
+
+    
+  },
+  plugins: [
+    
+  ],
+}
