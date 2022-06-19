b57f0a48afc65bdd6cc0f9886c0c1db412031832
abderrahmane jaafar <92488216+ABDELLPROO@users.noreply.github.com>
2022-06-19 17:54:25 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 module.exports = {
   content: [
     "./src/**/*.{html,js,php}",
@@ -28,34 +28,5 @@ module.exports = {
     
   ],
 }
-=======
-module.exports = {
-  content: [
-    "./src/**/*.{html,js,php}",
-    "./layouts/**/*.{html,js,php}",
-    "./pages/**/*.{html,js,php}"
-  ],
-  theme: {
-    extend: {
-      fontFamily:{
-        Heebo: "'Heebo', sans-serif ",
-        Mukta: "'Mukta', sans-serif ",
-        Bebas: "'Bebas Neue', cursive",
-        Montserrat: "'Montserrat', sans-serif",
-        Oswald: "'Oswald', sans-serif",
-        Exo: "'Exo', sans-serif",
-        Metrophobic: "'Metrophobic', sans-serif",
-        Quicksand: "'Quicksand', sans-serif",
-        Maven: "'Maven Pro', sans-serif",
-        Padauk: "'Padauk', sans-serif",
-        Darker_Grotesque: "'Darker Grotesque', sans-serif",
-      },
-    },
 
-    
-  },
-  plugins: [
-    
-  ],
-}
->>>>>>> c992ed9d32f8d5b9e4cc7af5b82ac93bfe171441
+
